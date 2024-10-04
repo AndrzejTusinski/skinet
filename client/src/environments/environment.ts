6d12d5d0a261ecc2bbf6d9da57d62e5cd2d152a1
Andrzej Tusinski <andyinq8@gmail.com>
2024-10-04 16:01:39 +0200

    Checkout

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'api/'
+  apiUrl: 'api/',
+  stripePublicKey: 'pk_test_51Q5v7UKDwU6JFf5gVYLOwRL4TvY1sVlWs9gXK94mToWEUSif2FZB0eqkVwWlkYYJu6FR3rTNjS41sBnqZeLQgRG600RZibrlho'
 };
