167f7f7b6626e28958a757c8e33e81a40a5ab16b
Andrzej Tusinski <andyinq8@gmail.com>
2024-10-05 13:51:41 +0200

    Finishing touches orders and payments

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:5001/api/',
+  hubUrl: 'https://localhost:5001/hub/notification/',
   stripePublicKey: 'pk_test_51Q5v7UKDwU6JFf5gVYLOwRL4TvY1sVlWs9gXK94mToWEUSif2FZB0eqkVwWlkYYJu6FR3rTNjS41sBnqZeLQgRG600RZibrlho'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
+  hubUrl: 'hub/notifications/',
   stripePublicKey: 'pk_test_51Q5v7UKDwU6JFf5gVYLOwRL4TvY1sVlWs9gXK94mToWEUSif2FZB0eqkVwWlkYYJu6FR3rTNjS41sBnqZeLQgRG600RZibrlho'
 };
