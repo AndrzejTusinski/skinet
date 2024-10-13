8c0aba2aec8aaf1605e6278f9fd83ccdba73eedd
Andrzej Tusinski <andyinq8@gmail.com>
2024-10-13 20:53:28 +0200

    Publishing to Azure

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'api/',
   hubUrl: 'hub/notifications/',
-  stripePublicKey: 'pk_test_51Q5v7UKDwU6JFf5gVYLOwRL4TvY1sVlWs9gXK94mToWEUSif2FZB0eqkVwWlkYYJu6FR3rTNjS41sBnqZeLQgRG600RZibrlho'
+  stripePublicKey:'pk_test_51Q9RFoAm2CsQyzKpvfJgvnT53mVQH5tvxipa2Gn5XprWYWj8xfxCzPaGa8sXp5JYaSVrRuomqoiDTlrQd1Uxjitl00Ak2M9vfE'
 };
